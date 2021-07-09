e3b95035202409d0604ed51682ec1f68b3e2836a
Derimar Gray <charminvidal@gmail.com>
2021-07-09 15:42:17 -0500

    ability of generate a  password with randoms numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -16,30 +16,42 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// function to write password
+// function to create password
 function generatePassword() {
   // ask for lenght of password between 8 and 128 characters
-  var question = parseInt(prompt("How many characters would you like your password?"));
+  var question = parseInt(prompt("How many characters would you like your password to have?"));
 
   // User interaction
   if (!question) {
     // if not character is input, do a remainder
     alert("You have to choose a number of characters!");
 
-  } else {
-    if (question < 8 || question > 128) 
-    // if character if input do a remainder
+  } else if (question < 8 || question > 128) {
+    // if character is not a valid input do a remainder
     var question = parseInt(prompt("You must choose between 8 and 128 characters"));
+
+  } else {
+    // ask validation of what kind of character will content
+    var questionNumbers = confirm("Will contain numbers?");
   };
+
+  if (!questionNumbers) {
+    option = alert("Required a option criteria!");
+  } else {
+    option = numbers;
+  }
 
   // array placeholder for generated length
   var passwordEl = [];
 
-  var password = passwordEl.join("");
-  UserInput(password);
-  return password;
-}
+  // Random selection of variables
+  for (var i = 0; i < question; i++) {
+    var pickOption = option[Math.floor(Math.random() * option.length)];
+    passwordEl.push(pickOption);
+  }
 
-function UserInput(password) {
-  document.getElementById("#password").textContent = password;
+  //convertion of the array to a string
+  var password = passwordEl.join("");
+
+  return password;
 }
