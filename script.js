17c40e5eee96219321a78a889432dbbc04545fd8
Derimar Gray <charminvidal@gmail.com>
2021-07-09 13:00:01 -0500

    ability to choose a length of characters between 8 and 128

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function to write password
+function generatePassword() {
+  // ask for lenght of password between 8 and 128 characters
+  var question = parseInt(prompt("How many characters would you like your password?"));
+
+  // User interaction
+  if (!question) {
+    // if not character is input, do a remainder
+    alert("You have to choose a number of characters!");
+
+  } else {
+    if (question < 8 || question > 128) 
+    // if character if input do a remainder
+    var question = parseInt(prompt("You must choose between 8 and 128 characters"));
+  };
+
+  // array placeholder for generated length
+  var passwordEl = [];
+
+  var password = passwordEl.join("");
+  UserInput(password);
+  return password;
+}
+
+function UserInput(password) {
+  document.getElementById("#password").textContent = password;
+}
